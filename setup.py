628deff03355f693b9bf1d0d2a8f1e2f759d2def
oczoske <ollicz68@yahoo.com>
2019-02-01 17:10:14 +0100

    Set version to 0.1.2

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def setup_package(version):
 if __name__ == '__main__':
     # Define version number
     major = 0
-    minor = 2
-    tiny = 0
+    minor = 1
+    tiny = 2
     version = "{}.{}.{}".format(major, minor, tiny)
     setup_package(version)
