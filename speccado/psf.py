329a648217405a1f36bc0fa380037a6cec28a02d
oczoske <ollicz68@yahoo.com>
2019-10-06 21:47:49 +0200

    PSF can be read from extension

--- speccado/psf.py
+++ speccado/psf.py
@@ -1,26 +1,60 @@
-'''Functions relating to the psf'''
+"""
+Functions relating to the psf
+
+These functions enhance the simcado.PSF class in a crude way
+"""
 
 import numpy as np
+
 from astropy.wcs import WCS
+from astropy.io import fits
+
 import simcado as sim
 
 # The contents of the two following functions should go into simcado
 # psf module
-def prepare_psf(psffile):
-    '''Create a SimCADO PSF object with added WCS and interpolation'''
-    psf = sim.psf.UserPSF(psffile)
+def prepare_psf(psffile, fits_ext=0):
+    """
+    Create a SimCADO PSF object with added WCS and interpolation
+
+    Parameters
+    ----------
+    psffile : path
+       File with one or more PSFs in separate extensions
+    fits_ext : int
+       Extension that holds the desired PSF. Default: 0
+
+    """
+    # Load the psf
+    psf = sim.psf.UserPSF(psffile, fits_ext=fits_ext)
+
+    # TODO: Check the units - we have confusion between arcsec and mas
     psf.array /= psf.pix_res**2      # normiert per arcsec2
-    psf.wcs = WCS(psffile)
+
+    # Add the WCS to the PSF object
+    psf.wcs = WCS(fits.getheader(psffile, ext=fits_ext))
+
+    # Add an interpolation function to the PSF object
     psf.interp = psf_interpolate(psf)
+
     return psf
 
 
 def psf_interpolate(psf):
-    '''Create interpolation function for a simcado PSF object'''
+    """
+    Create interpolation function for a simcado PSF object
+
+    Parameters
+    ----------
+    psf : instance of simcado.psf.UserPSF
+    """
     from scipy.interpolate import RectBivariateSpline
 
     # Pixel grid in python convention -> origin = 0
     xpix = np.arange(psf.shape[1])
     ypix = np.arange(psf.shape[0])
+
+    # Convert to angular coordinates (depends on the cunits of psf.wcs)
     xas, yas = psf.wcs.all_pix2world(xpix, ypix, 0)
+
     return RectBivariateSpline(xas, yas, psf.array)
